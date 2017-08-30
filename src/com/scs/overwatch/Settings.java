374d429133f1ea003bcec8e55516c7336ad793f4
Stephen Carlyle-Smith <stephen.carlylesmith@googlemail.com>
2017-08-30 07:28:39 +0100

    Tweaked gamepad

--- src/com/scs/overwatch/Settings.java
+++ src/com/scs/overwatch/Settings.java
@@ -7,7 +7,7 @@ public class Settings {
 	public static final boolean NEON = true;
 	public static final String VERSION = "0.01";
 	public static final boolean SHOW_LOGO = true;
-	public static final boolean ALWAYS_SHOW_4_CAMS = false;
+	public static final boolean ALWAYS_SHOW_4_CAMS = true;
 	public static final boolean RECORD_VID = false;
 	public static final boolean USE_MODEL_FOR_PLAYERS = true;
 	public static final boolean SHOW_FLASH_EXPLOSIONS = false;
