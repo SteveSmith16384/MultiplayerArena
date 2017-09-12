2bc1d9c2117f098057ba803bcc5d08c9cb8a680b
Stephen Carlyle-Smith <stephen.carlylesmith@googlemail.com>
2017-09-12 14:06:09 +0100

    Removed unused files

--- src/com/scs/overwatch/Settings.java
+++ src/com/scs/overwatch/Settings.java
@@ -4,13 +4,13 @@ import ssmith.lang.NumberFunctions;
 
 public class Settings {
 
-	public static final boolean NEON = true;
+	public static final boolean NEON = true; // todo - remove
 	public static final String VERSION = "0.01";
-	public static final boolean SHOW_LOGO = false;
+	public static final boolean SHOW_LOGO = true;
 	//public static final boolean SHOW_FPS = true;
 	public static final boolean ALWAYS_SHOW_4_CAMS = false;
 	public static final boolean RECORD_VID = false;
-	public static final boolean USE_MODEL_FOR_PLAYERS = false;
+	public static final boolean USE_MODEL_FOR_PLAYERS = true;
 	public static final boolean SHOW_FLASH_EXPLOSIONS = false;
 	public static final boolean PLAYER1_IS_MOUSE = true;
 
